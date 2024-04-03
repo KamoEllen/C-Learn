022b9bf592e6cf6ec35633959be80b679ca386df
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2024-04-03 14:39:39 +0200

    Create hello_world.cs

---
+++ hello_world.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic; //works for lists and collevtions
+using System.Linq; //works with data
+using System.Text; //text and coding
+using System.Threading.Tasks; //used for multi threaded apps
+
+namespace ProgrammingMoshHourLong
+{
+    internal class Program
+    {
+        static void Main(string[] args) // arggs is a default paameter , void means nothing,c# is case sensitive.
+
+        {
+            Console.WriteLine("Hello world"); 
+        }
+    }
+}
